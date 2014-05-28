c41baae17c062fbb0320f46ca9e8f87190cdedfa
Patrick van der Tuin <patricktuin@planet.nl>
2014-05-28 18:31:30 +0200

    pol

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,2 +1,7 @@
 class GiftsController < ApplicationController
+	before_filter :allow_iframe_requests
+
+	def allow_iframe_requests
+	  response.headers.delete('X-Frame-Options')
+	end
 end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,7 @@
 class PicturesController < ApplicationController
+	before_filter :allow_iframe_requests
+
+	def allow_iframe_requests
+	  response.headers.delete('X-Frame-Options')
+	end
 end
